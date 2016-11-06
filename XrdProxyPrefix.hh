5ea9e310cab8c8ffe42e05179c147776c2b5eb93
Jan <j.knedlik@gsi.de>
2016-11-06 03:32:52 +0100

    initial commit, fork from old XrdOpenLocal where all plugins have been combined

---
+++ XrdProxyPrefix.hh
@@ -0,0 +1,66 @@
+/********************************************************************************
+ *    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ *                                                                              *
+ *              This software is distributed under the terms of the             * 
+ *         GNU Lesser General Public Licence version 3 (LGPL) version 3,        *  
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+#ifndef __XRDREDIRCT_TOLOCAL_PLUGIN_HH___
+#define __XRDREDIRCT_TOLOCAL_PLUGIN_HH___
+#include <stdio.h>
+#include "XrdCl/XrdClPlugInInterface.hh"
+#include <algorithm>
+#include "XrdCl/XrdClFile.hh"
+#include "XrdCl/XrdClFileSystem.hh"
+#include "XrdCl/XrdClPlugInInterface.hh"
+#include "XrdCl/XrdClLog.hh"
+#include "XrdCl/XrdClDefaultEnv.hh"
+#include "XrdCl/XrdClURL.hh"
+#include "XrdOuc/XrdOucEnv.hh"
+#include "XrdOuc/XrdOucString.hh"
+#include "XrdVersion.hh"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <utility>
+extern XrdCl::Log XrdClRFSLog;
+
+
+namespace PPFactory {
+//----------------------------------------------------------------------------
+// Plugin factory
+//----------------------------------------------------------------------------
+class ProxyPrefixFactory: public XrdCl::PlugInFactory {
+	public:
+		//------------------------------------------------------------------------
+		// Constructore
+		//------------------------------------------------------------------------
+		ProxyPrefixFactory(const std::map<std::string, std::string> &config);
+		//------------------------------------------------------------------------
+		// Destructor
+		virtual ~ProxyPrefixFactory();
+
+		//------------------------------------------------------------------------
+		// Create a file plug-in for the given URL
+		//------------------------------------------------------------------------
+		virtual XrdCl::FilePlugIn *CreateFile( const std::string &url );
+
+		//------------------------------------------------------------------------
+		// Create a file system plug-in for the give/Lon URL
+		virtual XrdCl::FileSystemPlugIn *CreateFileSystem(const std::string &url);
+		//------------------------------------------------------------------------
+		// load default config:
+		void loadDefaultConf(std::map<std::string,std::string>& config);
+		//------------------------------------------------------------------------
+		
+
+	private:
+};
+};
+
+#endif // __XRDREDIRCT_TOLOCAL_PLUGIN_HH___
---
+++ makefile
@@ -0,0 +1,28 @@
+
+ifdef DEBUG
+DBG="debug"
+endif
+
+ifndef XRD_PATH
+$(error XRD_PATH is not set, set XRD_PATH to your XRootD installation)
+endif
+all:XrdOpenLocal.so
+ 	
+XrdOpenLocal.so: 
+	g++ -g3 -fPIC  -I$(XRD_PATH)/include/xrootd  -I./src/ -c *.cc -std=c++11
+	g++ -shared  -L$(XRD_PATH)/lib -Wl,-soname,XrdProxyPrefix.so.1,--export-dynamic -o XrdProxyPrefix.so *.o -lXrdUtils -lXrdCl
+	
+test: XrdOpenLocal.so
+	@./test/xrdcp_DEFAULT.sh $(DBG)
+	@./test/xrdcp_NODEFAULT.sh $(DBG)
+	
+clean:clean_o clean_lib clean_exe 
+
+clean_o:
+	@-rm  *.o 
+clean_lib:
+	@-rm *.so
+clean_exe:
+	@-rm -rf *.exe
+
+.PHONY: test
